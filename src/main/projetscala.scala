a59c1c7760e7360d132e3de2a564baf50bc20592
Marie635 <bienguig@gmail.com>
2021-10-27 15:17:38 +0200

    Projet scala

--- src/main/projetscala.scala
+++ src/main/projetscala.scala
@@ -1,7 +1,7 @@
 import scala.io.Source.fromResource
+
 // Ici nous créons une classe Tondeuse ayant comme attribut les coordonées x,y ainsi que l'orientation
 class Tondeuse(var x:Int, var y:Int, var orientation:String){
-
   //fonction qui affiche les éléments de la classe tondeuse
   override def toString ="la tondeuse a pour coordonnées ("+x+", "+y+", "+orientation+")"
 }
@@ -11,7 +11,7 @@ object Run extends App {
   /* La Fonction programmme prend en arguments la tondeuse et les instructions,
   exécutent chaque instruction et retourne les nouvelles coordonnées et orientation de la tondeuse*/
 
-  def programme(tondeuse: Tondeuse, instruction: List[String]): Unit = {
+  def programme(tondeuse: Tondeuse, instruction: List[String],pelouse:List[Int]): Unit = {
 
     for (lettre <- instruction) {
       //on verifie ici que les coordonnées de la tondeuse ne sont pas supérieures à celle de la pelouse
@@ -44,17 +44,17 @@ object Run extends App {
     }
     println(tondeuse)
   }
-
   //On cree une liste de liste nommée fichier qui prend les informations des tondeuses et de la pelouse
 
-  val fichier=List(List(5,5), List(1,2,"N"), List ("G","A","G","A","G","A","G","A","A"), List(3,3,"E"), List("A","A","D","A","A","D","A","D","D","A"))
+  val fichier = fromResource("ScalaFile.txt").getLines.toList
 
   // On crée des variables pour récupères chaque liste
-  val pelouse = fichier.head.map(_.toString.toInt)//La 1ère liste est la liste des coordonnées de la pelouse qu'on transforme en liste d'entier
-  val position1 = fichier(1)      // La 2ème est les coordonnées de la tondeuse 1
-  val instruction1 = fichier(2).map(_.toString)   // La 3ème correspond aux instructions de la tondeuse 1
-  val position2 = fichier(3)     // La 4ème est les coordonnées de la tondeuse 2
-  val instruction2 = fichier(4).map(_.toString) // La 5ème correspond aux instructions de la tondeuse 2
+  val pelouse = fichier.head.split(" ").toList.map(_.toInt)//La 1ère liste est la liste des coordonnées de la pelouse qu'on transforme en liste d'entier
+  val position1 = fichier(1).split(" ").toList      // La 2ème est les coordonnées de la tondeuse 1
+  val instruction1 = fichier(2).toList.map(_.toString)  // La 3ème correspond aux instructions de la tondeuse 1
+  val position2 = fichier(3).split(" ").toList   // La 4ème est les coordonnées de la tondeuse 2
+  val instruction2 = fichier(4).toList.map(_.toString) // La 5ème correspond aux instructions de la tondeuse 2
+
 
   //On traite les exceptions sur les coordonnées de la pelouse de l'orientation des tondeuses
   assert(pelouse.length==2, "Error: les coordonnées de la pelouse doivent etre x et y séparés par un espace")
@@ -87,12 +87,12 @@ object Run extends App {
 
   // On instancie 2 éléments de la classe tondeuse avec les paramètres du fichier
   //Je convertis les coordonnées en Int et les orientations en String
-  val Tondeuse1 = new Tondeuse(position1.head.toString.toInt, position1(1).toString.toInt, position1(2).toString)
-  val Tondeuse2 = new Tondeuse(position2.head.toString.toInt, position2(1).toString.toInt, position2(2).toString)
+  val Tondeuse1 = new Tondeuse(position1.head.toInt, position1(1).toInt, position1(2))
+  val Tondeuse2 = new Tondeuse(position2.head.toInt, position2(1).toInt, position2(2))
 
   //On exécute les instructions à travers la fonction programme
-  val p1= programme(Tondeuse1, instruction1)
-  val p2= programme(Tondeuse2, instruction2)
+  val p1= programme(Tondeuse1, instruction1,pelouse)
+  val p2= programme(Tondeuse2, instruction2,pelouse)
 }
 
 
